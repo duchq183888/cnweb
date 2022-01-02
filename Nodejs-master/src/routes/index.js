d1cb8ac0e9a912f7f606c0994bebd50b8e1ad798
Duc Quang <duc.hq183888@sis.hust.edu.vn>
2022-01-02 13:13:59 +0700

    update fe

---
+++ Nodejs-master/src/routes/index.js
@@ -0,0 +1,28 @@
+const loginRouter = require('./login');
+const signupRouter = require('./signup');
+const logoutRouter = require('./logout');
+const userRouter = require('./user');
+const siteRouter = require('./site');
+
+const authMiddleware = require('../middlewares/middleware');
+
+function route(app) {
+
+    // Site: all url not have to login to render
+    app.use('/', siteRouter);
+
+    // Auth: Page signup and login
+    app.use('/login', loginRouter);
+    app.use('/signup', signupRouter);
+
+    // user: After login
+    app.use('/user',authMiddleware.requireAuth ,userRouter);
+
+    // user want to logout
+    app.use('/logout', logoutRouter);
+    
+
+
+}
+
+module.exports = route;
